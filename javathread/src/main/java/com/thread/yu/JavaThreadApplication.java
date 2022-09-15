1c75550cd482f3d543258498f873f459061930aa
yu <zhangyunuc@163.com>
2022-09-15 19:46:54 +0800

    add java thread module

---
+++ javathread/src/main/java/com/thread/yu/JavaThreadApplication.java
@@ -0,0 +1,13 @@
+package com.thread.yu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaThreadApplication.class, args);
+    }
+
+}
